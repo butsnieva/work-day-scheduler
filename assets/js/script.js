d0f19d4eb6cb21192bb8789b39329652f46a51f6
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-26 00:40:37 -0500

    add textarea and button to the time row

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ $('#currentDay').text('Today is ' + moment().format('dddd, MMMM Do'))
 
 var startHour = 8
 
+
 for (i=0; i<9; i++) {
-    var timeRow = $('<div>').addClass('row')
-    var workHours = moment(startHour, 'h').format('LT')
-        timeRow.text(workHours)
-    $('.container').append(timeRow) 
+    var timeRow = $('<div>').addClass('row time-block')
+    var workingHour = $('<div>').addClass('hour col-1')
+    var time = moment(startHour, 'h').format('h A')
+        workingHour.text(time)
+    var textArea = $('<textarea>').addClass('col-9')
+    var saveBtn = $('<button>').addClass('saveBtn col-1')
+    $('.container').append(timeRow)
+    timeRow.append(workingHour, textArea, saveBtn)
     startHour++
 }
