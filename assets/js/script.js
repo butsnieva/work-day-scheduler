a03334bb62a41104d81992c58c9798996f200691
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-26 01:52:01 -0500

    add color-coded time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
 $('#currentDay').text('Today is ' + moment().format('dddd, MMMM Do'))
 
-var startHour = 8
+var startHour = 1
+var currentTime = parseInt(moment().format('h'))
+console.log(startHour)
 
 
-for (i=0; i<9; i++) {
+for (i=0; i<23; i++) {
     var timeRow = $('<div>').addClass('row time-block')
-    var workingHour = $('<div>').addClass('hour col-1')
-    var time = moment(startHour, 'h').format('h A')
-        workingHour.text(time)
-    var textArea = $('<textarea>').addClass('col-9')
-    var saveBtn = $('<button>').addClass('saveBtn col-1')
     $('.container').append(timeRow)
+
+    var workingHour = $('<div>').addClass('hour col-1')
+        var time = moment(startHour, 'h').format('h A')
+            workingHour.text(time)
+    var textArea = $('<textarea>').addClass('description col-9')
+    var saveBtn = $('<button>').addClass('saveBtn col-1')
     timeRow.append(workingHour, textArea, saveBtn)
+    
+        if (currentTime > startHour) {
+            $(textArea).addClass('past')
+        } else if (currentTime === startHour) {
+            $(textArea).addClass('present')
+        } else if (currentTime < startHour) {
+            $(textArea).addClass('future')
+        }
+
     startHour++
 }
