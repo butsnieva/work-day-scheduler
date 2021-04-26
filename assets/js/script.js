751ecd07237a791e84afda443c8efbb03b4c5b11
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-26 09:53:35 -0500

    load tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,38 @@
 $('#currentDay').text('Today is ' + moment().format('dddd, MMMM Do'))
 
-var startHour = 1
+var tasks = {}
 var currentTime = parseInt(moment().format('h'))
-console.log(startHour)
 
 
-for (i=0; i<23; i++) {
-    var timeRow = $('<div>').addClass('row time-block')
+var workHour = [8,9,10,11,12,13,14,15,16]
+for (i=0; i<workHour.length; i++) {
+    var timeRow = $('<div>').addClass('row time-block').attr('data-id', workHour[i])
     $('.container').append(timeRow)
 
     var workingHour = $('<div>').addClass('hour col-1')
-        var time = moment(startHour, 'h').format('h A')
+        var time = moment(workHour[i], 'h').format('h A')
             workingHour.text(time)
     var textArea = $('<textarea>').addClass('description col-9')
     var saveBtn = $('<button>').addClass('saveBtn col-1')
     timeRow.append(workingHour, textArea, saveBtn)
     
-        if (currentTime > startHour) {
-            $(textArea).addClass('past')
-        } else if (currentTime === startHour) {
-            $(textArea).addClass('present')
-        } else if (currentTime < startHour) {
-            $(textArea).addClass('future')
+        if (currentTime > workHour[i]) {
+            textArea.addClass('past')
+        } else if (currentTime === workHour[i]) {
+            textArea.addClass('present')
+        } else if (currentTime < workHour[i]) {
+            textArea.addClass('future')
         }
 
-    startHour++
 }
+
+tasks = []
+$('.saveBtn').on('click', function() {
+
+    var value = $(this).siblings('.description').val()
+    var time = $(this).parent('.row').data('id')
+    var task = {time: time, value: value}
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    //console.log(tasks)
+})
